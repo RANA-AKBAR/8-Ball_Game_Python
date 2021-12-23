e82e5f1ab56fc43fcee9db5efce4a890dafd8b25
pc <ranaakbar@mail.com>
2021-12-23 15:50:01 -0800

    initial

---
+++ 8ball.py
@@ -0,0 +1,10 @@
+import random
+
+#Users name and question
+name = "Rana"
+question = "how"
+answer = ""
+random_number = random.randint(1, 9)
+#Random number Generation stored in to varaible below
+
+# print(random_number)
